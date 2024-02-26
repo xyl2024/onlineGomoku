5f85fa897e3169d44f93039e10e9be8f68b8b362
xyl <alonely2021@qq.com>
2024-02-26 10:11:43 +0800

    matcher done, room %50

--- src/matcher.hpp
+++ src/matcher.hpp
@@ -10,84 +10,193 @@
 #include <condition_variable>
 namespace gomoku
 {
-/*玩家对战匹配使用的阻塞队列。*/
-template <class T>
-class MatchQueue
-{
-private:
-    std::list<T> _list;
-    std::mutex _mtx;
-    std::condition_variable _cond; // 阻塞消费线程，当_list.size() < 2时阻塞
-public:
-    /*获取队列元素个数*/
-    int Size()
+    /*玩家对战匹配使用的阻塞队列。*/
+    template <class T>
+    class MatchQueue
     {
-        std::unique_lock<std::mutex> lock(_mtx);
-        return _list.size();
-    }
-    /*判断是否为空*/
-    bool Empty()
-    {
-        std::unique_lock<std::mutex> lock(_mtx);
-        return _list.empty();
-    }
-    /*阻塞线程*/
-    void Wait()
-    {
-        std::unique_lock<std::mutex> lock(_mtx);
-        _cond.wait(lock);
-    }
-    /*数据入队，唤醒线程*/
-    bool Push(const T &data)
-    {
-        std::unique_lock<std::mutex> lock(_mtx);
-        _list.push_back(data);
-        // 唤醒线程
-        _cond.notify_all();
-        return true;
-    }
-    /*数据出队*/
-    bool Pop(T &data)
-    {
-        std::unique_lock<std::mutex> lock(_mtx);
-        if (_list.empty())
-            return false;
-        data = _list.front();
-        _list.pop_front();
-        return true;
-    }
-    /*删除指定数据*/
-    bool Remove(T &data)
-    {
-        std::unique_lock<std::mutex> lock(_mtx);
-        _list.remove(data);
-        return true;
-    }
-};
+    private:
+        std::list<T> _list;
+        std::mutex _mtx;
+        std::condition_variable _cond; // 阻塞消费线程，当_list.size() < 2时阻塞
+    public:
+        /*获取队列元素个数*/
+        int Size()
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            return _list.size();
+        }
+        /*判断是否为空*/
+        bool Empty()
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            return _list.empty();
+        }
+        /*阻塞线程*/
+        void Wait()
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _cond.wait(lock);
+        }
+        /*数据入队，唤醒线程*/
+        bool Push(const T &data)
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _list.push_back(data);
+            // 唤醒线程
+            _cond.notify_all();
+            return true;
+        }
+        /*数据出队*/
+        bool Pop(T &data)
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            if (_list.empty())
+                return false;
+            data = _list.front();
+            _list.pop_front();
+            return true;
+        }
+        /*删除指定数据*/
+        bool Remove(T &data)
+        {
+            std::unique_lock<std::mutex> lock(_mtx);
+            _list.remove(data);
+            return true;
+        }
+    };
 
-class Matcher
-{
-private:
-    MatchQueue<uint64_t> _bronzeQueue; // 青铜
-    std::thread _bronzeThread;
-    MatchQueue<uint64_t> _silverQueue; // 白银
-    std::thread _silverThread;
-    MatchQueue<uint64_t> _goldQueue;   // 黄金
-    std::thread _goldThread;
-    RoomManager* _rm;
-    UserTable* _ut;
-    OnlineUser* _ou;
-private:
-    /*线程入口函数*/
-    void BronzeRun();
-    void SilverRun();
-    void GoldRun();
-public:
-    Matcher(RoomManager* rm, UserTable* ut, OnlineUser* ou);
-    ~Matcher();
-    bool Add(uint64_t uid);
-    bool Del(uint64_t uid);
-    
-};
+#define BRONZE_SCORE 1000
+#define SILVER_SCORE 3000
+#define GOLD_SCORE 10000
+
+    /*玩家对战匹配管理*/
+    class Matcher
+    {
+    private:
+        MatchQueue<uint64_t> _bronzeQueue; // 青铜
+        std::thread _bronzeThread;
+        MatchQueue<uint64_t> _silverQueue; // 白银
+        std::thread _silverThread;
+        MatchQueue<uint64_t> _goldQueue; // 黄金
+        std::thread _goldThread;
+        RoomManager *_rm;
+        UserTable *_ut;
+        OnlineUser *_ou;
+
+    private:
+        void ThreadRunHelper(MatchQueue<uint64_t> &mq)
+        {
+            // 匹配线程无限循环
+            while(true)
+            {
+                // 1.队列人数小于2则阻塞等待
+                while(mq.Size() < 2) //循环判断
+                    mq.Wait();
+                // 2.出队两个玩家
+                uint64_t uid1, uid2;
+                bool ret = mq.Pop(uid1);
+                if(ret == false) 
+                    continue;
+                ret = mq.Pop(uid2);
+                if(ret == false)
+                {
+                    Add(uid1);
+                    continue;
+                }
+                // 3.判断两个玩家是否在游戏大厅在线
+                wsserver_t::connection_ptr conn1 = _ou->GetConnFromHall(uid1);
+                if(conn1.get()==nullptr)
+                {
+                    Add(uid2);
+                    continue;
+                }
+                wsserver_t::connection_ptr conn2 = _ou->GetConnFromHall(uid2);
+                if(conn2.get()==nullptr)
+                {
+                    Add(uid1);
+                    continue;
+                }
+                // 4.创建房间
+                room_ptr rp = _rm->CreateRoomForTwoUser(uid1, uid2);
+                if(rp.get() == nullptr)
+                {
+                    Add(uid1);
+                    Add(uid2);
+                    continue;
+                }
+                // 5.发送响应给两个玩家
+                Json::Value resp;
+                resp["optype"] = "match_success";
+                resp["result"] = true;
+                std::string body;
+                util::json::serialize(resp, body);
+                conn1->send(body);
+                conn2->send(body);
+            }
+        }
+        /*线程入口函数*/
+        void BronzeRun()
+        {
+            ThreadRunHelper(_bronzeQueue);
+        }
+        void SilverRun()
+        {
+            ThreadRunHelper(_silverQueue);
+        }
+        void GoldRun()
+        {
+            ThreadRunHelper(_goldQueue);
+        }
+
+    public:
+        Matcher(RoomManager *rm, UserTable *ut, OnlineUser *ou)
+            : _rm(rm), _ut(ut), _ou(ou), _bronzeThread(std::thread(&Matcher::BronzeRun, this)), _silverThread(std::thread(&Matcher::SilverRun, this)), _goldThread(std::thread(&Matcher::GoldRun, this))
+        {
+            mylog::INFO_LOG("玩家匹配管理模块初始化完成");
+        }
+        ~Matcher();
+        /*添加玩家到对应的阻塞队列中*/
+        bool Add(uint64_t uid)
+        {
+            // 1.获取玩家信息
+            Json::Value user;
+            bool ret = _ut->SelectById(uid, user);
+            if (ret == false)
+            {
+                mylog::INFO_LOG("获取玩家信息失败，uid: %d", uid);
+                return false;
+            }
+            int score = user["score"].asInt();
+            // 2.添加到对应队列
+            if (score < BRONZE_SCORE)
+                _bronzeQueue.Push(uid);
+            else if (score >= BRONZE_SCORE && score < SILVER_SCORE)
+                _silverQueue.Push(uid);
+            else
+                _goldQueue.Push(uid);
+            return true;
+        }
+        /*在对应的阻塞队列中删除玩家*/
+        bool Del(uint64_t uid)
+        {
+            // 1.获取玩家信息
+            Json::Value user;
+            bool ret = _ut->SelectById(uid, user);
+            if (ret == false)
+            {
+                mylog::INFO_LOG("获取玩家信息失败，uid: %d", uid);
+                return false;
+            }
+            int score = user["score"].asInt();
+            // 2.删除对应队列中的数据
+            if (score < BRONZE_SCORE)
+                _bronzeQueue.Remove(uid);
+            else if (score >= BRONZE_SCORE && score < SILVER_SCORE)
+                _silverQueue.Remove(uid);
+            else
+                _goldQueue.Remove(uid);
+            return true;
+        }
+    };
 }
 #endif
--- src/room.hpp
+++ src/room.hpp
@@ -6,8 +6,94 @@
 #include <unordered_map>
 namespace gomoku
 {
-    class Room;
-    using room_ptr = std::shared_ptr<Room *>;
+#define BOARD_ROW 15
+#define BOARD_COL 15
+    /*
+        房间类，负责玩家对战胜负的记录、聊天动作的处理等，
+        总之就是房间内任何动作都需要广播给所有在房间内的用户。
+    */
+    class Room
+    {
+        enum class room_status
+        {
+            GAME_START,
+            GAME_OVER
+        };
+
+    private:
+        uint64_t _rid;
+        room_status _status;
+        int _playerCnt;
+        uint64_t _whiteUid;
+        uint64_t _blackUid;
+        UserTable *_userTable;
+        OnlineUser *_onlineUser;
+        std::vector<std::vector<char>> _board;
+
+    public:
+        Room(uint64_t rid, UserTable *ut, OnlineUser *olu)
+            : _rid(rid), _status(room_status::GAME_START), _playerCnt(0), _userTable(ut), _onlineUser(olu), _board(BOARD_ROW, std::vector<char>(BOARD_COL, 0))
+        {
+        }
+        uint64_t GetRid()
+        {
+            return _rid;
+        }
+        room_status GetStatus()
+        {
+            return _status;
+        }
+        int GetPlayerCnt()
+        {
+            return _playerCnt;
+        }
+        uint64_t GetWhiteUid()
+        {
+            return _whiteUid;
+        }
+        uint64_t GetBlackUid()
+        {
+            return _blackUid;
+        }
+        void SetWhiteUid(uint64_t uid)
+        {
+            _whiteUid = uid;
+            _playerCnt++;
+        }
+        void SetBlackUid(uint64_t uid)
+        {
+            _blackUid = uid;
+            _playerCnt++;
+        }
+        /// 处理玩家请求
+        void HandleRequest(const Json::Value &req)
+        {
+            // 1.校验房间号是否匹配
+            Json::Value rsp;
+            // ...
+        }
+
+        /// 处理聊天动作
+        Json::Value HandleChat(const Json::Value &req)
+        {
+        }
+
+        /// 处理下棋动作
+        Json::Value HandleChess(const Json::Value &req)
+        {
+        }
+
+        /// 处理玩家退出房间动作
+        void HandleExitRoom(uint64_t uid)
+        {
+        }
+        /// 广播给房间内所有玩家
+        void Broadcast(const Json::Value &rsp)
+        {
+        }
+    };
+
+    using room_ptr = std::shared_ptr<Room>;
     /*
         房间管理类：负责创建房间、查找房间、销毁房间等与房间相关的工作。
         - 当两个玩家匹配成功，将为他们[创建房间]
@@ -64,88 +150,33 @@ namespace gomoku
         /// 删除房间中指定用户
 
         /// 给两个匹配成功的用户创建房间
-    };
-    /*
-        房间类，负责玩家对战胜负的记录、聊天动作的处理等，
-        总之就是房间内任何动作都需要广播给所有在房间内的用户。
-    */
-#define BOARD_ROW 15
-#define BOARD_COL 15
-    class Room
-    {
-        enum class room_status
+        room_ptr CreateRoomForTwoUser(uint64_t uid1, uint64_t uid2)
         {
-            GAME_START,
-            GAME_OVER
-        };
+            // 1. 判断两个玩家是否都在游戏大厅中
+            if (_onlineUser->InHall(uid1) == false)
+            {
+                mylog::INFO_LOG("用户不在游戏大厅中，uid: %lu", uid1);
+                return room_ptr();
+            }
+            if (_onlineUser->InHall(uid2) == false)
+            {
+                mylog::INFO_LOG("用户不在游戏大厅中，uid: %lu", uid2);
+                return room_ptr();
+            }
 
-    private:
-        uint64_t _rid;
-        room_status _status;
-        int _playerCnt;
-        uint64_t _whiteUid;
-        uint64_t _blackUid;
-        UserTable *_userTable;
-        OnlineUser *_onlineUser;
-        std::vector<std::vector<char>> _board;
+            // 2. 创建房间并将用户信息添加到房间中
+            std::unique_lock<std::mutex> lock(_mtx);
+            room_ptr rp(new Room(_nextRid, _userTable, _onlineUser));
+            rp->SetWhiteUid(uid1);
+            rp->SetBlackUid(uid2);
 
-    public:
-        Room(uint64_t rid, UserTable *ut, OnlineUser *olu)
-            : _rid(rid), _status(room_status::GAME_START), _playerCnt(0), _userTable(ut), _onlineUser(olu), _board(BOARD_ROW, std::vector<char>(BOARD_COL, 0))
-        {
+            // 3. 将房间信息用哈希表管理起来
+            _rooms.insert({_nextRid, rp});
+            _users.insert({uid1, _nextRid});
+            _users.insert({uid2, _nextRid});
+            _nextRid++;
+            return rp;
         }
-        uint64_t GetRid()
-        {
-            return _rid;
-        }
-        room_status GetStatus()
-        {
-            return _status;
-        }
-        int GetPlayerCnt()
-        {
-            return _playerCnt;
-        }
-        uint64_t GetWhiteUid()
-        {
-            return _whiteUid;
-        }
-        uint64_t GetBlackUid()
-        {
-            return _blackUid;
-        }
-        void SetWhiteUid(uint64_t uid)
-        {
-            _whiteUid = uid;
-            _playerCnt++;
-        }
-        void SetBlackUid(uint64_t uid)
-        {
-            _blackUid = uid;
-            _playerCnt++;
-        }
-        /// 处理玩家请求
-        void HandleRequest(const Json::Value& req)
-        {
-            // 1.校验房间号是否匹配
-            Json::Value rsp;
-            // ...
-        }
-
-        /// 处理聊天动作
-        Json::Value HandleChat(const Json::Value& req)
-        {}
-
-        /// 处理下棋动作
-        Json::Value HandleChess(const Json::Value& req)
-        {}
-
-        /// 处理玩家退出房间动作
-        void HandleExitRoom(uint64_t uid)
-        {}
-        /// 广播给房间内所有玩家
-        void Broadcast(const Json::Value& rsp)
-        {}
     };
 }
 #endif
